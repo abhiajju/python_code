51ca504c7058c6a760ce8aa7178eac87a9185c29
root <abhiajju09@gmail.com>
2019-06-27 07:27:40 +0530

    final done

--- goodle_srch.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import webbrowser as wb
-
-content = raw_input("enter content to be searched in google")
-wb.open_new_tab("https://www.google.com/search?q="+content)
-
--- try.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python2
-import commands
-
-f=open('/etc/hosts','a')
-for i in range(0,4):
-	msg='nodes[i].example.com'
-	f.write(msg)
-f.close()
